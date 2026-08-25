67b3a328c44a7b5ea905c07552d9e8f3a844a39b
thiepn <junsosmart@gmail.com>
2026-08-25 15:31:51 +0200

    Bump localization hardening build

--- enhancements.js
+++ enhancements.js
@@ -1,7 +1,7 @@
 'use strict';
 (() => {
   const current=document.currentScript;
-  const BUILD='20260825-stability5';
+  const BUILD='20260825-stability6';
   const assetUrl=name=>{const u=new URL(name,current?.src||location.href);u.searchParams.set('v',BUILD);return u.href};
   const topLevel=window.top===window;
   const UI_LANGUAGE_KEY='tms60-ui-language-v1';
