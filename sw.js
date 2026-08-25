42424f2ed5e6933b57833a3690131b26d96d37b6
thiepn <junsosmart@gmail.com>
2026-08-25 13:12:41 +0200

    Cache complete TMS60 QoL bundle

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE='tms60-qol-word-nav2-2026-08-25';
-const CORE=['./','./index.html','./app.html','./translations.js','./niv-service.json','./enhancements.js','./enhancements-legacy.js','./rc3-hardening.js','./qol-word-navigation.js','./enhancements-core.js','./localization-runtime.js','./localization-completion.js','./favicon.svg','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
+const CACHE='tms60-qol-bundle1-2026-08-25';
+const CORE=['./','./index.html','./app.html','./translations.js','./niv-service.json','./enhancements.js','./enhancements-legacy.js','./rc3-hardening.js','./qol-fast-recall.js','./qol-cloze-helpers.js','./qol-word-navigation.js','./enhancements-core.js','./localization-runtime.js','./localization-completion.js','./favicon.svg','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
     const cache=await caches.open(CACHE);
