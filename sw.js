1cf3b43ddfd4312a8975e27fa1f42e05f11ab0d7
thiepn <junsosmart@gmail.com>
2026-08-25 17:12:24 +0200

    Refresh PWA cache for localization event isolation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='tms60-stability8-2026-08-25';
+const CACHE='tms60-stability9-2026-08-25';
 const CORE=['./','./index.html','./app.html','./translations.js','./niv-service.json','./enhancements.js','./enhancements-legacy.js','./rc3-hardening.js','./language-switch-hardening.js','./guided-learning-chain.js','./qol-fast-recall.js','./qol-cloze-helpers.js','./qol-word-navigation.js','./enhancements-core.js','./localization-runtime.js','./localization-completion.js','./favicon.svg','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
